975581478e368d7c58e49c29df6455d53e7ee606
Adem Demir <93316402+AdemDemirFE@users.noreply.github.com>
2024-12-23 01:24:55 +0300

    *

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -34,6 +34,16 @@ export class LoginPage implements OnInit {
     },
   ];
 
+  isModalOpen = false;
+  selectedStory: any;
+  progress = 0;
+  storyIndex = 0;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+
   toggleCard(index: number) {
     this.activeCard = this.activeCard === index ? null : index;
   }
@@ -43,5 +53,37 @@ export class LoginPage implements OnInit {
     console.log('Buton tıklandı!');
   }
 
-  ngOnInit() {}
+
+  openModal(index: number) {
+    this.storyIndex = index;
+    this.selectedStory = this.history[index];
+    this.isModalOpen = true;
+    this.startStoryTimer();
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.progress = 0;
+  }
+
+  startStoryTimer() {
+    this.progress = 0;
+    const interval = setInterval(() => {
+      this.progress += 1;
+      if (this.progress >= 100) {
+        clearInterval(interval);
+        this.nextStory();
+      }
+    }, 90); // 90ms * 100 = 9 seconds
+  }
+
+  nextStory() {
+    if (this.storyIndex < this.history.length - 1) {
+      this.storyIndex++;
+      this.selectedStory = this.history[this.storyIndex];
+      this.startStoryTimer();
+    } else {
+      this.closeModal(); // Tüm hikayeler bittiğinde modal kapanır
+    }
+  }
 }
